b62b1dc72aa9920736ea374cde888f68a1a73b45
jonathantower <jonathantower@gmail.com>
2014-09-08 18:03:59 -0400

    Initial push

---
+++ HtmlHelperExtensions.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebPages;
+
+namespace System.Web.Mvc
+{
+    public enum AssetHierarchy
+    {
+        Layout = 0,
+        Page = 1,
+        Partial = 2
+    }
+
+    public static class HtmlHelperExtensions
+    {
+        private class Asset
+        {
+            public AssetHierarchy AssetHierarchy { get; set; }
+            public string Url { get; set; }
+        }
+
+        private static List<Asset> Scripts
+        {
+            get
+            {
+                if (!HttpContext.Current.Items.Contains("user-request-script-items"))
+                {
+                    HttpContext.Current.Items["user-request-script-items"] = new List<Asset>();
+                }
+
+                return (HttpContext.Current.Items["user-request-script-items"] as List<Asset>);
+            }
+        }
+
+        private static List<Asset> Styles
+        {
+            get
+            {
+                if (!HttpContext.Current.Items.Contains("user-request-style-items"))
+                {
+                    HttpContext.Current.Items["user-request-style-items"] = new List<Asset>();
+                }
+
+                return (HttpContext.Current.Items["user-request-style-items"] as List<Asset>);
+            }
+        }
+
+        /// <summary>
+        /// Renders the scripts onto the page that were added by the view and partial views.
+        /// </summary>
+        /// <param name="html">The HTMLHelper</param>
+        /// <returns></returns>
+        public static MvcHtmlString RenderScripts(this HtmlHelper html)
+        {
+            var sb = new StringBuilder();
+            foreach (var script in Scripts.OrderBy(i => i.AssetHierarchy))
+            {
+                sb.AppendLine(String.Format("<script src=\"{0}\"></script>", UrlHelper.GenerateContentUrl(script.Url, html.ViewContext.HttpContext)));
+            }
+            return new MvcHtmlString(sb.ToString());
+        }
+
+        /// <summary>
+        /// Renders the stylesheets onto the page that were added by the view and partial views.
+        /// </summary>
+        /// <param name="html">The HTMLHelper</param>
+        /// <returns></returns>
+        public static MvcHtmlString RenderStyleSheets(this HtmlHelper html)
+        {
+            var sb = new StringBuilder();
+            //Styles.Reverse();
+            foreach (var style in Styles.OrderBy(i => i.AssetHierarchy))
+            {
+                sb.AppendLine(String.Format("<link rel=\"stylesheet\" type=\"text/css\" href=\"{0}\">", UrlHelper.GenerateContentUrl(style.Url, html.ViewContext.HttpContext)));
+            }
+            return new MvcHtmlString(sb.ToString());
+        }
+
+        /// <summary>
+        /// Adds a script to be emitted by the RenderScripts method.
+        /// </summary>
+        /// <param name="html">The HTMLHelper.</param>
+        /// <param name="scriptUrl">The script URL.</param>
+        /// <param name="scriptUrl">The display priority.</param>
+        public static void AddScript(this HtmlHelper html, string scriptUrl, AssetHierarchy hierarchy = AssetHierarchy.Page)
+        {
+            Scripts.Add(new Asset { Url = scriptUrl  , AssetHierarchy = hierarchy});
+        }
+
+        /// <summary>
+        /// Adds a stylesheet to be emitted by the RenderStyleSheets method.
+        /// </summary>
+        /// <param name="html">The HTMLHelper</param>
+        /// <param name="styleUrl">The stylesheet URL.</param>
+        /// <param name="scriptUrl">The display priority.</param>
+        /// 
+        public static void AddStyleSheet(this HtmlHelper html, string styleUrl, AssetHierarchy hierarchy = AssetHierarchy.Page)
+        {
+            Styles.Add(new Asset { Url = styleUrl, AssetHierarchy = hierarchy });
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Falafel.RenderScripts")]
+[assembly: AssemblyDescription("Allows you to emit scripts and stylesheets into a layout from views and partial views")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Falafel Software, Inc.")]
+[assembly: AssemblyProduct("RenderScripts")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("138e476f-e9c0-4c9b-ac40-02ecf4841ae3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.2.*")]
+[assembly: AssemblyFileVersion("0.2.*")]
